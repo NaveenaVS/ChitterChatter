2928f8b651896773a02cb8e21c9bac303e57e767
Naveena <vishnaveena@gmail.com>
2020-10-09 10:55:44 -0500

    login
    implemented login to fetch and display my twitter account

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Naveena vishnu on 10/8/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //creating connection to screen's button as an action
+    @IBAction func onLoginButton(_ sender: Any) {
+       //we want to have a login!!
+        //in starter we have the set up of the api call to twitter already created so we use that and code:
+        let myURL = "https://api.twitter.com/oauth/request_token" //called specific endpoint that controls our login
+        TwitterAPICaller.client?.login(url: myURL, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Login failed!")
+        }) //the api caller
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
